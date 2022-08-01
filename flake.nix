c9584f3294325f0c7d35dcda228b2e7ed960e1ef
Matthew Toohey <contact@mtoohey.com>
2022-08-01 18:30:58 -0400

    build: improve overlay overridability and remove poetry2nix input

--- flake.nix
+++ flake.nix
@@ -4,53 +4,41 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.flake-utils.follows = "utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, utils, poetry2nix }:
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ poetry2nix.overlay ];
-          };
-        in
-        with pkgs; {
-          packages = rec {
-            default = (pkgs.poetry2nix.mkPoetryApplication {
-              projectDir = ./.;
-            }).overrideAttrs (oldAttrs: {
-              postFixup = oldAttrs.postFixup + ''
-                substituteInPlace $out/lib/python3.10/site-packages/lf_exa_icons/main.py \
-                  --replace 'build/tree-sitter-rust.so' '${tree-sitter-grammars.tree-sitter-rust}/parser'
-              '';
-            });
-
-            lf-exa-icons-output = runCommand "lf-exa-icons-output" { } ''
-              ${default}/bin/lf-exa-icons ${exa.src}/src/output/icons.rs > $out
-            '';
-          };
-
-          devShells.default = (pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-          }).overrideAttrs (oldAttrs: {
-            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-              poetry
-              (python3Packages.python-lsp-server.overrideAttrs (_: {
-                # TODO: remove this once it works again
-                doInstallCheck = false;
-              }))
-            ];
-          });
-        }) // {
-      overlays.default = (final: _: {
-        lf-exa-icons = self.packages.${final.system}.default;
-        lf-exa-icons-output = self.packages.${final.system}.lf-exa-icons-output;
+  outputs = { self, nixpkgs, utils }: {
+    overlays.default = final: _: rec {
+      lf-exa-icons = (final.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+      }).overrideAttrs (oldAttrs: {
+        postFixup = oldAttrs.postFixup + ''
+          substituteInPlace $out/lib/python3.10/site-packages/lf_exa_icons/main.py \
+            --replace 'build/tree-sitter-rust.so' '${final.tree-sitter-grammars.tree-sitter-rust}/parser'
+        '';
       });
+      lf-exa-icons-output = final.runCommand "lf-exa-icons-output" { } ''
+        ${lf-exa-icons}/bin/lf-exa-icons ${final.exa.src}/src/output/icons.rs > $out
+      '';
     };
+  } //
+  utils.lib.eachDefaultSystem (system: with import nixpkgs
+    { inherit system; overlays = [ self.overlays.default ]; } ; {
+    packages = {
+      default = lf-exa-icons;
+      inherit lf-exa-icons-output;
+    };
+
+    devShells.default = (poetry2nix.mkPoetryEnv {
+      projectDir = ./.;
+    }).overrideAttrs (oldAttrs: {
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+        poetry
+        python3
+        (python3Packages.python-lsp-server.overrideAttrs (_: {
+          # TODO: remove this once it works again
+          doInstallCheck = false;
+        }))
+      ];
+    });
+  });
 }
