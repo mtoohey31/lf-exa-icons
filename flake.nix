774088ad4f56deee12382bf183509e4964f8a293
Matthew Toohey <contact@mtoohey.com>
2023-01-06 22:51:14 -0500

    build: update flake

--- flake.nix
+++ flake.nix
@@ -23,20 +23,21 @@
             --replace 'build/tree-sitter-rust.so' '${final.tree-sitter-grammars.tree-sitter-rust}/parser'
         '';
       });
+
       lf-exa-icons-output = final.runCommand "lf-exa-icons-output" { } ''
         ${lf-exa-icons}/bin/lf-exa-icons ${final.exa.src}/src/output/icons.rs > $out
       '';
     };
-  } //
-  utils.lib.eachDefaultSystem (system: with import nixpkgs
+  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
     { inherit system; overlays = [ self.overlays.default ]; } ; {
     packages = {
       default = lf-exa-icons;
-      inherit lf-exa-icons-output;
+      inherit lf-exa-icons lf-exa-icons-output;
     };
 
     devShells.default = lf-exa-icons.dependencyEnv.overrideAttrs (oldAttrs: {
       nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+        gcc
         poetry
         python3
         python3Packages.python-lsp-server
